82c346e0900805bf1cf271746a28f0e17078a577
Sean Tolbert <stolbert4150@gmail.com>
2022-07-12 19:13:22 -0400

    trials timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { colours } from "nodemon/lib/config/defaults";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const stuff = [
+    { main: "neat", value: "start" },
+    { main: "neato", value: "start" },
+    { main: "neaty", value: "start" },
+    { main: "neati", value: "start" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+  ];
 
   return (
-    <div className="App text-cyan-200">
-      WOAH dere
+    <div className="h-screen flex justify-between items-center w-full">
+      {stuff.map((item) => (
+        <div className="flex flex-col items-center">
+          <span className="h-20 w-20 bg-slate-400 rounded-md opacity-0"></span>
+          <span className="peer z-[10] h-4 w-4 bg-black rounded-full"></span>
+          <span className="h-20 w-20 bg-slate-400 rounded-md opacity-0 peer-hover:opacity-100 transition duration-300">date</span>
+        </div>
+      ))}
+      <span className="absolute w-full bg-black h-1"></span>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
