e4d3423eeb8f20cc8dae37ccd5bcca5ae1a6b4f0
Sean Tolbert <stolbert4150@gmail.com>
2022-07-14 09:04:52 -0400

    builds out card with title and adds menu button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { colours } from "nodemon/lib/config/defaults";
 import { useState } from "react";
 import "./App.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -14,22 +15,54 @@ function App() {
     { main: "neatly", value: "end" },
     { main: "neatly", value: "end" },
     { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
+    { main: "neatly", value: "end" },
   ];
 
   return (
-    <div className="h-screen flex justify-between items-center w-full">
-      {stuff.map((item) => (
-        <div className="group flex flex-col items-center">
-          <span className="h-20 w-20 bg-slate-400 flex justify-center items-center rounded-md opacity-0 group-hover:opacity-100 transition duration-300 group-hover:-translate-y-5">
-            <img src="https://i.imgur.com/M4mu25f.png" alt="" className=""/>
-          </span>
-          <span className="peer z-[10] h-4 w-4 bg-black rounded-full hover:h-6 hover:w-6 transition-all duration-300"></span>
-          <span className="h-20 w-20 bg-slate-400 group-hover:translate-y-5 rounded-md opacity-0 group-hover:opacity-100 transition duration-300 flex justify-center items-center">
-            date
-          </span>
+    <div className="h-96 flex justify-between items-center w-full">
+      {stuff.map((item, index) => (
+        <div
+          key={index}
+          className="h-full group flex flex-col justify-center items-center"
+        >
+          <div className="z-[10] h-10 w-10 flex justify-center items-center bg-white rounded-full group-hover:h-6 group-hover:w-6 transition-all duration-300 opacity-100 group-hover:opacity-0 text-2xl">
+            <FontAwesomeIcon icon={faGithub} />
+          </div>
+          <div className="h-20 w-0 p-1 group-hover:h-56 group-hover:w-48 flex flex-col justify-center items-center rounded-md opacity-0 group-hover:opacity-100 transition-all duration-300 ">
+            <div className="flex justify-between w-full ">
+              <div className="text-lg text-teal-300">Title here right now boiii thats right im looking at you</div>
+              {/* remove when not logged in */}
+              <div className="mr-2 text-white text-xl">
+                <FontAwesomeIcon icon={faEllipsis} />
+              </div>
+            </div>
+
+            <img
+              src="https://i.imgur.com/M4mu25f.png"
+              alt="example image"
+              className="rounded-lg"
+            />
+
+            <div className="opacity-0 group-hover:opacity-100 transition duration-300 delay-100 flex flex-col">
+              <span className="text-xs text-white">about a month ago</span>
+              <span className="text-white">We have a description here. and what a description it is, i mean do you feel pretty described??</span>
+            </div>
+          </div>
         </div>
       ))}
-      <span className="absolute w-full bg-black h-1"></span>
+      <span className="z-[-1] absolute w-screen bg-white -translate-y-10 h-0.5"></span>
     </div>
   );
 }
